472626f0128002e03f32a9a34ae045c22a2181ea
robalexg <alexg7070@yahoo.com>
2022-10-18 12:44:34 -0500

    sign up from styling

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -3,7 +3,21 @@ import './Signup.css'
 const Signup = () => {
     return (
         <header className='signup--container'>
-            <h1>Signuppppp</h1>
+            <div className='signup--banner'>
+                <h1>Try it free 7 days <span className='light-text'>then $20/mo. thereafter</span></h1>
+            </div>
+            <form className='signup--form'>
+                <input type="text" placeholder='First Name'/>
+                <input type="text" placeholder='Last Name'/>
+                <input type="text" placeholder='Email Address'/>
+                <input type="text" placeholder='Password'/>
+                <input type="text" placeholder='Confirm Password'/>
+                <button className='signup--button' >CLAIM YOUR FREE TRIAL</button>
+                <p>
+                    By clicking the button, you are agreeing to our Terms 
+                    and Services
+                </p>
+            </form>
         </header>
     )
 }
